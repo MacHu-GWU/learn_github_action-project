16dff2026bf1aa28691b836bec3e4eb641bb8334
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-23 15:12:27 -0500

    Update show_rich_color.py

--- examples/github_action_ui/show_rich_color.py
+++ examples/github_action_ui/show_rich_color.py
@@ -2,8 +2,8 @@
 
 from rich.console import Console
 
-console = Console()
-# console = Console(force_terminal=True)
+# console = Console() # this works in terminal, but won't show color in GitHub action
+console = Console(force_terminal=True) # this can show color in GitHub action
 
 data = {
     "name": "Show rich color",
